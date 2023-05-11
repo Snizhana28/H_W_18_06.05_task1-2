6ba8bef700b38928a38a9ab6c9c323ba3c7f2177
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-05-11 17:20:18 +0300

    application.cpp

--- application.cpp
+++ application.cpp
@@ -1,5 +1,26 @@
+#include "child/parrot.h"
+#include "child/cat.h"
+#include "child/dog.h"
+
+#include <vector>
+
 int main()
 {
+	cout << "Pet" << endl;
+	vector<Pet*> pets;
+	pets.push_back(new Parrot());
+	pets.push_back(new Dog());
+	pets.push_back(new Cat());
 
+	for (int i = 0; i < pets.size(); i++)
+	{
+		pets[i]->print();
+		cout << endl;
+	}
+	for (int i = 0; i < pets.size(); i++)
+	{
+		delete pets[i];
+	}
+	pets.clear();
 	return 0;
 }
